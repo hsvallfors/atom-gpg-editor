3f7931296b12ec27087a2727a09ff5907cc11850
Hugo Svallfors <hugo@pugglepay.com>
2015-12-13 20:29:26 +0100

    Created basic Lein cljs setup.

---
+++ README.md
@@ -0,0 +1,17 @@
+# atom-gpg-editor
+
+An Atom plugin for reading and editing GPG files.
+
+FIXME: examples of usage
+
+## Installation
+
+FIXME: installation instructions
+
+## Contribute
+
+FIXME: contributor guidelines / how to build
+
+## License
+
+[MIT License](https://github.com/keiter/atom-gpg-editor/blob/master/LICENSE.md)
---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject atom-gpg-editor "0.1.0"
+
+  :description "An Atom plugin for reading and editing GPG files."
+
+  :license {:name "MIT Licence"
+            :url "https://github.com/keiter/atom-gpg-editor/blob/master/LICENCE.md"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [lein-cljsbuild "1.1.1"]]
+
+  :plugins [[lein-cljsbuild "1.1.1"]]
+
+  :cljsbuild
+   {:builds
+     {:main
+       {:source-paths ["src"]
+        :compiler
+         {:output-to "./lib/atom-gpg-editor.js"
+          :optimizations :simple
+          :target :nodejs
+          :pretty-print true}}}})
