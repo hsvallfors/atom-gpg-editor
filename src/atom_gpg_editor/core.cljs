07d0a2898fd08f29bc8f926a13ba631e67440801
Hugo Svallfors <hugo@zimpler.com>
2016-07-06 22:00:51 +0200

    Added modal panel that can receive user password.

--- src/atom_gpg_editor/core.cljs
+++ src/atom_gpg_editor/core.cljs
@@ -1,10 +1,15 @@
 (ns atom-gpg-editor.core
   (:require
     [atom-gpg-editor.child_process :as child]
-    [clojure.string :as string]))
+    [clojure.string :as string]
+    [hipo.core :as hipo]
+    [dommy.core :as dommy]))
 
 (def observers
   (atom []))
+
+(def panel
+  (atom nil))
 
 (defn add-observer!
   [obs]
@@ -14,9 +19,15 @@
   []
   (doall (map #(.dispose %) @observers)))
 
+(defn dispose-panel!
+  []
+  (when @panel
+    (.destroy @panel)
+    (reset! panel nil)))
+
 (defn atom-confirm!
   [message]
-  (.confirm js/atom (js-obj "message" message)))
+  (.confirm js/atom (clj->js {:message message})))
 
 (defn will-save-buffer
   [text-buffer]
@@ -24,11 +35,25 @@
     (child/spawn! "gpg" ["--help"])
     :stdout string/split-lines first atom-confirm!))
 
+(def gpg-cancel dispose-panel!)
+
+(defn gpg-confirm
+  [mini-editor]
+  (atom-confirm! (str "Password was " (.getText mini-editor))))
+
 (defn created-text-editor
   [editor]
   (when (string/ends-with? (.getPath editor) ".gpg")
-    (atom-confirm! "You opened a GPG file!")
-    (add-observer! (.. editor getBuffer (onWillSave will-save-buffer)))))
+    (let [mini-editor (-> {:mini true :placeholderText "GPG Password"}
+                          clj->js
+                          js/atom.workspace.buildTextEditor)
+          panel-div (hipo/create [:div.atom-gpg-editor])]
+      (dommy/append! panel-div (js/atom.views.getView mini-editor))
+      ; FIXME hide password input
+      (->> {:item panel-div} clj->js js/atom.workspace.addModalPanel (reset! panel))
+      (js/atom.commands.add panel-div "core:confirm" #(gpg-confirm mini-editor))
+      (js/atom.commands.add panel-div "core:cancel" #(gpg-cancel))
+      (add-observer! (.. editor getBuffer (onWillSave will-save-buffer))))))
 
 (defn activate
   []
@@ -36,12 +61,14 @@
 
 (defn deactivate
   []
-  (dispose-observers!))
+  (dispose-observers!)
+  (dispose-panel!))
 
 (set! js/module.exports
-  (js-obj "activate" activate
-          "deactivate" deactivate
-          "serialize" (constantly nil)))
+  (clj->js
+    {:activate activate
+     :deactivate deactivate
+     :serialize (constantly nil)}))
 
 ;; noop - needed for :nodejs CLJS build
 (set! *main-cli-fn* (constantly nil))
